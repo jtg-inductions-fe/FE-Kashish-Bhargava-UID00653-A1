87565743d7924e4c94a2db548abf3797cdeae422
kashishbhargava03 <kashish.bhargava@joshtechnologygroup.com>
2025-09-09 15:53:17 +0530

    Updations in Sidebar

--- src/script/index.js
+++ src/script/index.js
@@ -3,8 +3,8 @@ const sidebar = document.getElementById('sidebar');
 const hamburger = document.querySelector('.header__hamburger');
 const closeSidebar = document.getElementById('closeSidebar');
 hamburger.addEventListener('click', () => {
-    sidebar.classList.add('active');
+    sidebar.classList.toggle('active');
 });
 closeSidebar.addEventListener('click', () => {
-    sidebar.classList.remove('active');
+    sidebar.classList.toggle('active');
 });
